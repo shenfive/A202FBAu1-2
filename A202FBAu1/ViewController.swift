7932a3cd38e4a4a9b86d9cb9f47651335da67a79
Danny Shen <shenfive@gmail.com>
2022-03-20 11:39:39 +0800

    timing

--- A202FBAu1/ViewController.swift
+++ A202FBAu1/ViewController.swift
@@ -8,14 +8,26 @@
 import UIKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
+    
+    //FB 登入完成
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        print("登入完成 FB")
+    }
+    
+    //登出
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("登出 FB")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let loginButton = FBLoginButton()
-                loginButton.center = view.center
-                view.addSubview(loginButton)
+        loginButton.delegate = self
+        loginButton.center = view.center
+        view.addSubview(loginButton)
     }
 
 
